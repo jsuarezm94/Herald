74d3ea5ecdf26aaeaee7f9a31e22772e19a15ce5
jctellez <jc.tellez19@yahoo.com>
2016-04-12 17:00:36 -0400

    added code to enable swipe to delete for outbox table VC, but the geotifications are not being properly deleted from the data source

--- Herald/Herald/OutboxMessagesTableViewController.swift
+++ Herald/Herald/OutboxMessagesTableViewController.swift
@@ -64,17 +64,22 @@ class OutboxMessagesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
+            
+            tableView.beginUpdates()
             // Delete the row from the data source
+            geotificationsList?.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        
+            tableView.endUpdates()
+            
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
